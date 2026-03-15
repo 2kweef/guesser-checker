7cd753199f591f58e6a62db4cb9096a539515189
Toufeek Siddique <toufeek@Toufeeks-MacBook-Air.local>
2026-03-15 06:46:36 +0000

    Completed number guesser game with while loop

--- guesser.py
+++ guesser.py
@@ -5,12 +5,17 @@ tries = 0
 guess = 0
 
 print("What number am I thinking of between 1 and 20?")
-text = input("Take a guess: ")
 
-guess = int(text)
-tries = tries + 1
 
-if guess == secret:
-    print("Correct")
-else:
-    text = input("Incorrect, try again: ")
+while guess != secret:
+
+    guess = int(input("Please enter a number: "))
+
+    tries = tries + 1
+
+    if guess < secret:
+        print("Too low")
+    elif guess > secret:
+        print("Too high")
+    else:
+        print("Correct! You got it in " + str(tries) + " tries!")
